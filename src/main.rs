a49a836c5d01105b03098b4c61d25f8be40e1400
小煜 <galgamecloud@qq.com>
2024-01-21 13:44:40 +0800

    Refactor main.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-#![allow(unsafe_code)]
+// Test that we can paint to the screen using glow directly.
 
-use eframe::{egui, egui_glow, epaint::Vec2, glow};
+mod utils;
 
-use egui::mutex::Mutex;
-use std::sync::Arc;
+use eframe::glow;
+use eframe::{egui, epaint::Vec2};
 
-fn main() -> Result<(), eframe::Error> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        // Let's show off that we support transparency:
+        renderer: eframe::Renderer::Glow,
         viewport: egui::ViewportBuilder {
             min_inner_size: Some(Vec2::new(1366.0, 768.0)),
             ..Default::default()
@@ -20,7 +19,8 @@ fn main() -> Result<(), eframe::Error> {
         "Egui+GL学习",
         options,
         Box::new(|cc| Box::new(MyApp::new(cc))),
-    )
+    )?;
+    Ok(())
 }
 
 fn setup_custom_fonts(ctx: &egui::Context) {
@@ -44,179 +44,30 @@ fn setup_custom_fonts(ctx: &egui::Context) {
     ctx.set_fonts(font);
 }
 
-struct MyApp {
-    /// Behind an `Arc<Mutex<…>>` so we can pass it to [`egui::PaintCallback`] and paint later.
-    rotating_triangle: Arc<Mutex<RotatingTriangle>>,
-    angle: f32,
-    name: String,
-    age: u32,
-}
+#[derive(Default)]
+struct MyApp {}
 
 impl MyApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         setup_custom_fonts(&cc.egui_ctx);
-        let gl = cc.gl.as_ref().expect("绘制Eframe需要启用glow");
-        Self {
-            rotating_triangle: Arc::new(Mutex::new(RotatingTriangle::new(gl))),
-            angle: 0.0,
-            name: "小煜".to_owned(),
-            age: 42,
-        }
-    }
-
-    fn custom_painting(&mut self, ui: &mut egui::Ui) {
-        let (rect, response) =
-            ui.allocate_exact_size(egui::Vec2::splat(300.0), egui::Sense::drag());
-
-        self.angle += response.drag_delta().x * 0.01;
-
-        // Clone locals so we can move them into the paint callback:
-        let angle = self.angle;
-        let rotating_triangle = self.rotating_triangle.clone();
-
-        let callback = egui::PaintCallback {
-            rect,
-            callback: std::sync::Arc::new(egui_glow::CallbackFn::new(move |_info, painter| {
-                rotating_triangle.lock().paint(painter.gl(), angle);
-            })),
-        };
-        ui.painter().add(callback);
+        Self {}
     }
 }
 
 impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("你好世界!这是小米字体。");
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        use glow::HasContext as _;
+        let gl = frame.gl().unwrap();
 
-            ui.horizontal(|ui| {
-                let name_label = ui.label("你的名字: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("岁"));
-            if ui.button("点击增加一岁").clicked() {
-                self.age += 1;
-            }
+        #[allow(unsafe_code)]
+        unsafe {
+            gl.viewport(0, 0, 100, 100);
+            gl.clear_color(0.0, 0.0, 0.0, 0.0); // purple
+            gl.clear(glow::COLOR_BUFFER_BIT);
+        }
 
-            egui::Frame::canvas(ui.style()).show(ui, |ui| {
-                self.custom_painting(ui);
-            });
-            ui.label(format!("你好！ '{}', 年龄 {}", self.name, self.age));
+        egui::Window::new("浮动窗口").show(ctx, |ui| {
+            ui.label("这是背景上的浮动窗口");
         });
-    }
-
-    fn on_exit(&mut self, gl: Option<&glow::Context>) {
-        if let Some(gl) = gl {
-            self.rotating_triangle.lock().destroy(gl);
-        }
-    }
-}
-
-struct RotatingTriangle {
-    program: glow::Program,
-    vertex_array: glow::VertexArray,
-}
-
-impl RotatingTriangle {
-    fn new(gl: &glow::Context) -> Self {
-        use glow::HasContext as _;
-
-        let shader_version = "#version 330".to_string();
-
-        unsafe {
-            let program = gl.create_program().expect("无法创建着色器程序");
-
-            let (vertex_shader_source, fragment_shader_source) = (
-                r#"
-                    const vec2 verts[3] = vec2[3](
-                        vec2(0.0, 1.0),
-                        vec2(-1.0, -1.0),
-                        vec2(1.0, -1.0)
-                    );
-                    const vec4 colors[3] = vec4[3](
-                        vec4(1.0, 0.0, 0.0, 1.0),
-                        vec4(0.0, 1.0, 0.0, 1.0),
-                        vec4(0.0, 0.0, 1.0, 1.0)
-                    );
-                    out vec4 v_color;
-                    uniform float u_angle;
-                    void main() {
-                        v_color = colors[gl_VertexID];
-                        gl_Position = vec4(verts[gl_VertexID], 0.0, 1.0);
-                        gl_Position.x *= cos(u_angle);
-                    }
-                "#,
-                r#"
-                    precision mediump float;
-                    in vec4 v_color;
-                    out vec4 out_color;
-                    void main() {
-                        out_color = v_color;
-                    }
-                "#,
-            );
-
-            let shader_sources = [
-                (glow::VERTEX_SHADER, vertex_shader_source),
-                (glow::FRAGMENT_SHADER, fragment_shader_source),
-            ];
-
-            let shaders: Vec<_> = shader_sources
-                .iter()
-                .map(|(shader_type, shader_source)| {
-                    let shader = gl.create_shader(*shader_type).expect("无法创建着色器");
-                    gl.shader_source(shader, &format!("{shader_version}\n{shader_source}"));
-                    gl.compile_shader(shader);
-                    assert!(
-                        gl.get_shader_compile_status(shader),
-                        "Failed to compile {shader_type}: {}",
-                        gl.get_shader_info_log(shader)
-                    );
-                    gl.attach_shader(program, shader);
-                    shader
-                })
-                .collect();
-
-            gl.link_program(program);
-            assert!(
-                gl.get_program_link_status(program),
-                "{}",
-                gl.get_program_info_log(program)
-            );
-
-            for shader in shaders {
-                gl.detach_shader(program, shader);
-                gl.delete_shader(shader);
-            }
-
-            let vertex_array = gl.create_vertex_array().expect("无法创建顶点数组对象");
-
-            Self {
-                program,
-                vertex_array,
-            }
-        }
-    }
-
-    fn destroy(&self, gl: &glow::Context) {
-        use glow::HasContext as _;
-        unsafe {
-            gl.delete_program(self.program);
-            gl.delete_vertex_array(self.vertex_array);
-        }
-    }
-
-    fn paint(&self, gl: &glow::Context, angle: f32) {
-        use glow::HasContext as _;
-        unsafe {
-            gl.use_program(Some(self.program));
-            gl.uniform_1_f32(
-                gl.get_uniform_location(self.program, "u_angle").as_ref(),
-                angle,
-            );
-            gl.bind_vertex_array(Some(self.vertex_array));
-            gl.draw_arrays(glow::TRIANGLES, 0, 3);
-        }
     }
 }
